e7c5ac447b51dc01dd61bf7a52eb8c0ce11d86de
Konstantin <156708735+genr89@users.noreply.github.com>
2024-03-02 16:53:10 +0300

    Update calc.go

--- calc.go
+++ calc.go
@@ -2,85 +2,102 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-var romanNumerals = map[rune]int{
-	'I': 1,
-	'V': 5,
-	'X': 10,
-	'L': 50,
-	'C': 100,
-	'D': 500,
-	'M': 1000,
-}
+// Функция для преобразования римских чисел в десятичные
+func romanToDecimal(roman string) int {
+	romanMap := map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
+	}
 
-func romanToArabic(roman string) int {
-	result := 0
+	var result int
+	prevValue := 0
 
-	for i := 0; i < len(roman); i++ {
-		currentValue := romanNumerals[rune(roman[i])]
-
-		if i+1 < len(roman) {
-			nextValue := romanNumerals[rune(roman[i+1])]
-
-			if currentValue < nextValue {
-				result += nextValue - currentValue
-				i++ // Пропустить следующий символ, так как уже учтен
-			} else {
-				result += currentValue
-			}
+	for i := len(roman) - 1; i >= 0; i-- {
+		value := romanMap[string(roman[i])]
+		if value < prevValue {
+			result -= value
 		} else {
-			result += currentValue
+			result += value
 		}
+		prevValue = value
 	}
 
 	return result
 }
 
-func arabicToRoman(arabic int) string {
-	result := ""
+// Функция для преобразования десятичных чисел в римские
+func decimalToRoman(decimal int) string {
+	romanMap := map[int]string{
+		1:    "I",
+		4:    "IV",
+		5:    "V",
+		9:    "IX",
+		10:   "X",
+		40:   "XL",
+		50:   "L",
+		90:   "XC",
+		100:  "C",
+		400:  "CD",
+		500:  "D",
+		900:  "CM",
+		1000: "M",
+	}
 
-	for _, numeral := range []struct {
-		Value  int
-		Symbol string
-	}{
-		{1000, "M"},
-		{900, "CM"},
-		{500, "D"},
-		{400, "CD"},
-		{100, "C"},
-		{90, "XC"},
-		{50, "L"},
-		{40, "XL"},
-		{10, "X"},
-		{9, "IX"},
-		{5, "V"},
-		{4, "IV"},
-		{1, "I"},
-	} {
-		for arabic >= numeral.Value {
-			result += numeral.Symbol
-			arabic -= numeral.Value
+	var result strings.Builder
+	values := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+
+	for _, value := range values {
+		for decimal >= value {
+			result.WriteString(romanMap[value])
+			decimal -= value
 		}
 	}
 
-	return result
-}
-
-func addRomanNumbers(roman1, roman2 string) string {
-	arabic1 := romanToArabic(roman1)
-	arabic2 := romanToArabic(roman2)
-	sum := arabic1 + arabic2
-
-	return arabicToRoman(sum)
+	return result.String()
 }
 
 func main() {
-	// Пример использования II + II = IV
-	romanNumber1 := "II"
-	romanNumber2 := "II"
+	var input1, input2 string
+	var operator rune
 
-	sum := addRomanNumbers(romanNumber1, romanNumber2)
+	fmt.Println("Привет! Добро пожаловать в римский калькулятор.")
+	fmt.Println("Введите первое римское число:")
+	fmt.Scanln(&input1)
+	fmt.Println("Введите оператор (+, -, *, /):")
+	fmt.Scanln(&operator)
+	fmt.Println("Введите второе римское число:")
+	fmt.Scanln(&input2)
 
-	fmt.Printf("%s + %s = %s\n", romanNumber1, romanNumber2, sum)
+	number1 := romanToDecimal(input1)
+	number2 := romanToDecimal(input2)
+
+	var result int
+
+	switch operator {
+	case '+':
+		result = number1 + number2
+	case '-':
+		result = number1 - number2
+	case '*':
+		result = number1 * number2
+	case '/':
+		if number2 == 0 {
+			fmt.Println("Ошибка: деление на ноль")
+			return
+		}
+		result = number1 / number2
+	default:
+		fmt.Println("Ошибка: неверный оператор")
+		return
+	}
+
+	fmt.Println("Результат:", decimalToRoman(result))
 }
